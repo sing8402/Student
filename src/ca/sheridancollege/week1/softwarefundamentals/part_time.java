7eebc9c8637067a62703d005cd6321068f7fde3f
JASKARAN <JASKARAN@jaskaran>
2019-05-24 14:10:27 -0400

    firstcommpart

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/part_time.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author JASKARAN
+ */
+public class part_time {
+   int _numberOfHours ;
+   
+   public double getNUmberOfhours(){
+       return _numberOfHours;
+   }
+    
+}
